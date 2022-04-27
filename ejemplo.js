95c14f2c49aaaca0fcc4e1dc1eddaef07f09d45e
Ballesta44 <103542598+Ballesta44@users.noreply.github.com>
2022-04-26 21:37:32 -0300

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -1,8 +1,16 @@
 
-
 let Numero = Number(prompt("Elegí un Número"))
 if (!Number.isNaN(Numero)) {
     alert("Bien")
 } else {
     alert("Dale pelotudo")
+    alert("Bueno, vamos de nuevo")
+    let Letra =  prompt("Is this the real life?")
+    if (Letra == "Is this just fantasy?" || "is this just fantasy?") {
+        alert("Ta, te perdono")
+    } else {
+        alert("Cancelado")
+        self.close()
+    }
 }
+
