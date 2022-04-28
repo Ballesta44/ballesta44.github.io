a319fd7937be91eae551e2a6324d1b5a5f655f6b
Ballesta44 <103542598+Ballesta44@users.noreply.github.com>
2022-04-28 14:16:38 -0300

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -1,20 +1,30 @@
 
 
-
-
 function prueba() {
-let Numero = Number(prompt("Elegí un Número"))
+let numero = Number(prompt("Elegí un Número"))
 if (!Number.isNaN(Numero) && Numero != "") {
     alert("Bien")
 } else {
     alert("Dale pelotudo")
     alert("Bueno, vamos de nuevo")
-    let Letra =  prompt("Is this the real life?")
-    if (Letra.toLowerCase() == "is this just fantasy?") {
+    let letra =  prompt("Is this the real life?")
+    if (letra.toLowerCase() == "is this just fantasy?") {
         alert("Ta, te perdono")
     } else {
         alert("Cancelado")
         document.write("CANCELAAAAAAAADO")
     }
 }
+}
+
+    var coso = 0
+    function set(){
+    document.getElementById("variable").innerHTML = coso
+    }
+    function contador() {
+    coso = coso + 1
+    document.getElementById("variable").innerHTML = coso
+    document.getElementById("comentario").innerHTML = "qué irresponsable"
+    document.getElementById("variable").contentWindow.location.reload(true);
+    document.getElementById("comentario").contentWindow.location.reload(true);
 }
