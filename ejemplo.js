7475641e135c7ca3319218c2b470724532ec0f37
Ballesta44 <103542598+Ballesta44@users.noreply.github.com>
2022-04-28 15:36:51 -0300

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -1,29 +1,29 @@
 
 
 function prueba() {
-let numero = Number(prompt("Elegí un Número"))
-if (!Number.isNaN(numero) && numero != "") {
+    let numero = Number(prompt("Elegí un Número"))
+    if (!Number.isNaN(numero) && Numero != "") {
     alert("Bien")
-} else {
+    } else {
     alert("Dale pelotudo")
     alert("Bueno, vamos de nuevo")
     let letra =  prompt("Is this the real life?")
     if (letra.toLowerCase() == "is this just fantasy?") {
         alert("Ta, te perdono")
     } else {
-        alert("Cancelado")
-        document.write("CANCELAAAAAAAADO")
+        /*alert("Cancelado")
+    document.write("CANCELAAAAAAAADO")*/
+    alert("Ah si? Tomá")
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"}
     }
-}
-}
+};
 
-    var coso = 0
-    function set(){
+let coso = 0;
+function set(){
     document.getElementById("variable").innerHTML = coso
     }
-    function contador() {
+function contador() {
     coso = coso + 1
     document.getElementById("variable").innerHTML = coso
     document.getElementById("comentario").innerHTML = "qué irresponsable"
-
-}
+};
